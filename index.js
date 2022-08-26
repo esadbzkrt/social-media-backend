62039f3391d16343f5c2bb65b9210d2fa5e01869
esadbzkrt <esadbzkrt@gmail.com>
2022-08-27 01:18:05 +0300

    Update

--- index.js
+++ index.js
@@ -5,9 +5,36 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+dotenv.config();
 
+const PORT = process.env.PORT;
+const MONGO = process.env.MONGO;
+
+mongoose.connect(MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, () => {
+    console.log('Connected to MongoDB');
+
+}
+);
+
+// Middleware
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+
+}
+);
+
+
+
+app.listen(PORT, () => {
+    console.log("Server started on port", `${PORT}`);
 }
 );
 
